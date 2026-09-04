2abb5acaaac34c430c6a8a2d34da249f97659534
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-09-04 20:34:39 +0600

    added signup and login page

---
+++ src/Components/authentication/SignUpForm.jsx
@@ -0,0 +1,113 @@
+"use client";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@heroui/react";
+import Link from "next/link";
+import React from "react";
+
+const SignUpForm = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+  };
+  return (
+    <div>
+      <Form className="flex w-full flex-col gap-4" onSubmit={onSubmit}>
+        <TextField isRequired name="name" type="text">
+          <Label>নাম</Label>
+          <Input placeholder="আপনার নাম..." />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>ইমেইল</Label>
+          <Input placeholder="আপনার ইমেইল..." />
+          <FieldError />
+        </TextField>
+        <TextField isRequired name="profileImage" type="text">
+          <Label>প্রোফাইল ছবি</Label>
+          <Input placeholder="আপনার প্রোফাইল ছবির URL..." />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          defaultValue="12345678"
+        >
+          <Label>পাসওয়ার্ড</Label>
+          <Input placeholder="আপনার পাসওয়ার্ড লিখুন..." />
+          <Description>
+            পাসওয়ার্ড কমপক্ষে ৮ অক্ষরের হতে হবে, একটি বড় হাতের অক্ষর এবং একটি
+            সংখ্যা থাকতে হবে。
+          </Description>
+          <FieldError />
+        </TextField>
+
+        <div className="flex flex-col gap-4">
+          <Label isRequired>রোল</Label>
+          <RadioGroup
+            defaultValue="user"
+            name="role"
+            orientation="horizontal"
+            isRequired
+          >
+            <Radio value="user">
+              <Radio.Content>
+                <Radio.Control>
+                  <Radio.Indicator className="border rounded-full" />
+                </Radio.Control>
+                ব্যবহারকারী
+              </Radio.Content>
+            </Radio>
+            <Radio value="admin">
+              <Radio.Content>
+                <Radio.Control>
+                  <Radio.Indicator className="border rounded-full" />
+                </Radio.Control>
+                এডমিন
+              </Radio.Content>
+            </Radio>
+          </RadioGroup>
+        </div>
+
+        <div className="flex gap-2">
+          <Button type="submit" className="w-full mb-6 bg-sky-900">
+            রেজিস্টার
+          </Button>
+        </div>
+      </Form>
+
+      <div className="mt-6">
+        <p className="mt-5">
+          একাউন্ট আছে?{" "}
+          <Link href="/login" className="underline text-blue-600">
+            লগইন করুন
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/app/(authentication)/signup/page.jsx
+++ src/app/(authentication)/signup/page.jsx
@@ -1,7 +1,25 @@
+import SignUpForm from "@/Components/authentication/SignUpForm";
+import { Card } from "@heroui/react";
 import React from "react";
 
 const page = () => {
-  return <div>signup page</div>;
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-400px)] mt-10">
+      <Card className="w-lg mx-auto p-10" variant="default">
+        <Card.Header>
+          <Card.Title className="text-2xl font-bold ">
+            রেজিস্টার করুন
+          </Card.Title>
+          <Card.Description>
+            রেজিস্টার করতে এখানে আপনার তথ্য প্রবেশ করুন
+          </Card.Description>
+        </Card.Header>
+        <Card.Content>
+          <SignUpForm />
+        </Card.Content>
+      </Card>
+    </div>
+  );
 };
 
 export default page;
